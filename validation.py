f6eb15f4a7088367959d9c4c1f230ebec813bdaa
vpershukov <somenewbox@gmail.com>
2019-08-25 14:26:50 +0300

    validation changed

--- validation.py
+++ validation.py
@@ -56,10 +56,8 @@ def import_validation(citizens):
 
         birth_date_validation(citizen["birth_date"])
 
-        gender = citizen["gender"].split()
-        if len(gender) != 1:
-            abort(400)
-        if not gender[0] or gender[0] not in ("male", "female"):
+        gender = citizen["gender"]
+        if not gender or gender not in ("male", "female"):
             abort(400)
 
         list_relatives = citizen["relatives"]
@@ -120,10 +118,8 @@ def update_citizen_validation(citizen):
         if field == "gender":
             if not isinstance(citizen["gender"], str):
                 abort(400)
-            gender = citizen["gender"].split()
-            if len(gender) != 1:
-                abort(400)
-            if not gender[0] or gender[0] not in ("male", "female"):
+            gender = citizen["gender"]
+            if gender not in ("male", "female"):
                 abort(400)
 
         if field == "relatives":
